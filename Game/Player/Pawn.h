4feb86c731c507900a3f880a5a6c5447dde95e05
Magma <moorefieldautodetailing@gmail.com>
2025-07-17 22:50:57 -0400

    Some globals, and Possess offset

--- Game/Player/Pawn.h
+++ Game/Player/Pawn.h
@@ -13,7 +13,18 @@ enum class EFortCustomPartType : uint8_t
 	EFortCustomPartType_MAX = 7
 };
 
-class FortnitePlayerPawn : public FortnitePlayerPawn
+class FortnitePlayerPawn
 {
-    void ServerChoosePart(EFortCustomBodyType Part, UObject* CharacterPart);
+	public: 
+
+    void ServerChoosePart(EFortCustomBodyType Part, UObject* CharacterPart) /*@TODO TMR*/
+	{
+
+	}
+
+	static void Possess(UObject* NController, UObject* NPawn) //pretty sure this is needed
+	{
+		auto PossessPath = FindFunction("Function /Script/Engine.Controller.Possess", nullptr);
+		ProcessEventO(NController, PossessPath, NPawn);
+	}
 };
---
+++ Game/globals.h
@@ -0,0 +1,11 @@
+#include "Engine/other.h"
+#include "Engine/structs.h"
+
+namespace Globals
+{
+    UObject* GWorld;
+    UObject* Pawn;
+    UObject* GameState;
+    UObject* GameMode;
+    UObject* PlayerState;
+}
